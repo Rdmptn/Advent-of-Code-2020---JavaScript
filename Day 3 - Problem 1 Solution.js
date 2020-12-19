7478cd10355c930dccee346af6a1573b57b74a92
Cameron <66243209+Rdmptn@users.noreply.github.com>
2020-12-18 20:58:15 -0500

    Update Day 3 - Problem 1 Solution.js

--- Day 3 - Problem 1 Solution.js
+++ Day 3 - Problem 1 Solution.js
@@ -3,6 +3,7 @@
 let hill_width =  (Math.ceil((hill.length * 3) / 31));
 //setting witdth to an arbitrary value greater than what is needed to solve the problem, 
 //could change 31 to a variable to adjust for dynamic width instead of manually counting
+//hill refers to the array of strings given in the problem
 var count = 0;
 var trees = 0;
 
